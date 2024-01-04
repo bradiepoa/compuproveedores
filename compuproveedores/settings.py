1aab33114eb370de882a814ce4d96353fb6e3291
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-01-04 17:46:51 +0300

    first templates config

--- compuproveedores/settings.py
+++ compuproveedores/settings.py
@@ -45,7 +45,7 @@ ROOT_URL = "http://162.243.44.17/"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
